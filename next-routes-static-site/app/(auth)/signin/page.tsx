9ac2b7743bbcc17cb73e51eed6df9087f89ba12f
Saurabh <saurabh.404dev@gmail.com>
2025-08-09 18:41:54 +0530

    route groups

---
+++ next-routes-static-site/app/(auth)/signin/page.tsx
@@ -0,0 +1,8 @@
+export default function (){
+    return  <div>
+        Signin
+        <input type="text" />
+        <input type="text" />
+        <button>sign In</button>
+    </div>
+}
---
+++ next-routes-static-site/app/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function (){
+    return  <div>
+        SignUp
+        <input type="text" />
+        <input type="text" />
+        <button>sign Up</button>
+    </div>
+}
